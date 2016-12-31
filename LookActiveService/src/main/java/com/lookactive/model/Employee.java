2438d3fc15dda389c13e8c396d2f2f0a83f3d6b9
Rishi Kyamaji <kyamajir@gmail.com>
2016-12-31 12:14:53 +0530

    delete and put service methods added

---
+++ LookActiveService/src/main/java/com/lookactive/model/Employee.java
@@ -0,0 +1,26 @@
+package com.lookactive.model;
+
+public class Employee {
+	private int empId;
+	private String empName;
+	private String empAddress;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpAddress() {
+		return empAddress;
+	}
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+}
--- LookActiveService/src/main/java/com/lookactive/service/LookActiveService.java
+++ LookActiveService/src/main/java/com/lookactive/service/LookActiveService.java
@@ -1,18 +1,56 @@
 package com.lookactive.service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.lookactive.model.Employee;
 
 @Path("/v1")
 public class LookActiveService {
 
 	@GET
-	@Path("/getTest")
-	@Produces("text/html")
-	public String getTest(){
-		return "test";
+	@Path("/employee/{empid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Employee getTest(@PathParam("empid") int employeeId){
+		Employee emp = new Employee();
+		emp.setEmpId(employeeId);
+		emp.setEmpName("Rahul");
+		emp.setEmpAddress("koramangala");
+		return emp;
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	@Path("/employee")
+	public String postTest(Employee emp){
+		return emp.getEmpName();
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	@Path("/employee/{empid}")
+	public String putTest(@PathParam("empid") String empId ,Employee emp)
+	{
+		return  "updated employee with id:"+empId+"\n"+"with name: "+emp.getEmpName()+" - residing in address: "+emp.getEmpAddress();
+		
+	}
+	
+	@DELETE
+	@Produces(MediaType.TEXT_HTML)
+	@Path("/employee/{emp}")
+	public String deleteTest(@PathParam("emp") String empId)
+	{
+		return "deleting emp with id :"+empId;
+		
 	}
 }
